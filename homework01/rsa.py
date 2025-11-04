a548f988376e5518bad2d4debcc8346a1baa12ef
valeria <lerag200711@gmail.com>
2025-11-05 00:54:35 +0300

    Исправлены ошибки

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -57,7 +57,10 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     _, x, _ = extended_gcd(e, phi)
     return x % phi
 
-def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
+
+def generate_keypair(
+    p: int, q: int
+) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
     if not (is_prime(p) and is_prime(q)):
         raise ValueError("Both numbers must be prime.")
     elif p == q:
