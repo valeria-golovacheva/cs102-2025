7f0ef5cc866786574d3a10f5a40e5b82fa947fb0
valeria <lerag200711@gmail.com>
2025-11-05 00:17:11 +0300

    Исправлены ошибки

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -14,14 +14,14 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     key_index = 0
 
     for char in plaintext:
-        if 'A' <= char <= 'Z':
-            shift = ord(keyword[key_index % key_length]) - ord('A')
-            encrypted_char = chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+        if "A" <= char <= "Z":
+            shift = ord(keyword[key_index % key_length]) - ord("A")
+            encrypted_char = chr((ord(char) - ord("A") + shift) % 26 + ord("A"))
             ciphertext += encrypted_char
             key_index += 1
-        elif 'a' <= char <= 'z':
-            shift = ord(keyword[key_index % key_length]) - ord('A')
-            encrypted_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+        elif "a" <= char <= "z":
+            shift = ord(keyword[key_index % key_length]) - ord("A")
+            encrypted_char = chr((ord(char) - ord("a") + shift) % 26 + ord("a"))
             ciphertext += encrypted_char
             key_index += 1
         else:
@@ -47,14 +47,14 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     key_index = 0
 
     for char in ciphertext:
-        if 'A' <= char <= 'Z':
-            shift = ord(keyword[key_index % key_length]) - ord('A')
-            decrypted_char = chr((ord(char) - ord('A') - shift + 26) % 26 + ord('A'))
+        if "A" <= char <= "Z":
+            shift = ord(keyword[key_index % key_length]) - ord("A")
+            decrypted_char = chr((ord(char) - ord("A") - shift + 26) % 26 + ord("A"))
             plaintext += decrypted_char
             key_index += 1
-        elif 'a' <= char <= 'z':
-            shift = ord(keyword[key_index % key_length]) - ord('A')
-            decrypted_char = chr((ord(char) - ord('a') - shift + 26) % 26 + ord('a'))
+        elif "a" <= char <= "z":
+            shift = ord(keyword[key_index % key_length]) - ord("A")
+            decrypted_char = chr((ord(char) - ord("a") - shift + 26) % 26 + ord("a"))
             plaintext += decrypted_char
             key_index += 1
         else:
