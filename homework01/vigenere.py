0a9b8d43ae874c6e0db78fa46b0cbd5b6d52bb51
valeria <lerag200711@gmail.com>
2025-11-04 22:39:29 +0300

    Реализованы функции encrypt_vigenere() и decrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,7 +9,25 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    key_length = len(keyword)
+    key_index = 0
+
+    for char in plaintext:
+        if 'A' <= char <= 'Z':
+            shift = ord(keyword[key_index % key_length]) - ord('A')
+            encrypted_char = chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+            ciphertext += encrypted_char
+            key_index += 1
+        elif 'a' <= char <= 'z':
+            shift = ord(keyword[key_index % key_length]) - ord('A')
+            encrypted_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+            ciphertext += encrypted_char
+            key_index += 1
+        else:
+            ciphertext += char
+            key_index += 1
+
     return ciphertext
 
 
@@ -24,5 +42,22 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    key_length = len(keyword)
+    key_index = 0
+
+    for char in ciphertext:
+        if 'A' <= char <= 'Z':
+            shift = ord(keyword[key_index % key_length]) - ord('A')
+            decrypted_char = chr((ord(char) - ord('A') - shift + 26) % 26 + ord('A'))
+            plaintext += decrypted_char
+            key_index += 1
+        elif 'a' <= char <= 'z':
+            shift = ord(keyword[key_index % key_length]) - ord('A')
+            decrypted_char = chr((ord(char) - ord('a') - shift + 26) % 26 + ord('a'))
+            plaintext += decrypted_char
+            key_index += 1
+        else:
+            plaintext += char
+            key_index += 1
     return plaintext
